d581fcaa4093300cf30996172637a79fd47a1d02
Tim Chuppuev <chuppuev@outlook.com>
2019-12-05 22:45:26 +0400

    Лабораторная работа 1

---
+++ WindowsFormsLainers/WindowsFormsLainers/FormShip.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsLainers
+{
+    public partial class FormShip : Form
+    {
+        private Lainer ship;
+
+        public FormShip()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxShips.Width, pictureBoxShips.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            ship.DrawShip(gr);
+            pictureBoxShips.Image = bmp;
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            ship = new Lainer(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Red, Color.Gray, true, true, true, true);
+            ship.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxShips.Width, pictureBoxShips.Height);
+            Draw();
+        }
+
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    ship.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown":
+                    ship.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    ship.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight":
+                    ship.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+
+    }
+}
